6bd64c9578885ef4057904e8be06333be5b05fbf
habilmb <habilmediob@gmail.com>
2026-05-25 13:41:06 +0700

    remove dead imports

--- backend/agent/llm_agent.py
+++ backend/agent/llm_agent.py
@@ -2,7 +2,6 @@ from langchain_community.llms import Ollama
 from langchain_groq import ChatGroq
 from langchain_community.tools import DuckDuckGoSearchRun
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain import PromptTemplate
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
